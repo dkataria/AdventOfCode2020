30dd4d66775522f71fdca3c858d0f1e55b9d19c5
gadak <drk8921@g.rit.edu>
2020-12-10 20:18:10 -0500

    'reverting accidentally checked in session id'

--- AocGetInput.py
+++ AocGetInput.py
@@ -6,7 +6,7 @@ def getInput(day):
     url = 'https://adventofcode.com/2020/day/' + str(day) + '/input'
 
     # set cookie below as your session id from your browser session to AOC
-    cookie = '53616c7465645f5f7a2362b2e67fc9ca3ad6f34e84359616c7097d5e7f116bb18b37ca30b4b1fcb387475e063af2e7ae'
+    cookie = ''
 
     headers = {'session': cookie}
     session = requests.Session()
